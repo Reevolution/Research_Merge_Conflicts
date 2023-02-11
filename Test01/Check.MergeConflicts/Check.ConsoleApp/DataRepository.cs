7b064dfda689cfed01352f5725842dd04d4e1a85
Reevolution <reevolutionvega@yandex.com>
2023-02-11 12:50:41 +0300

    Инициализация (#1)
    Co-authored-by: KoleschukEA <KoleschukEA@sibintek.ru>

---
+++ Test01/Check.MergeConflicts/Check.ConsoleApp/DataRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check.ConsoleApp
+{
+    public class DataRepository
+    {
+        private const string FirstValue = "First Value";
+        private const string SecondValue = "Second Value";
+
+        public string GetFirstValue()
+        {
+            return FirstValue;
+        }
+
+        public string GetSecondValue()
+        {
+            return SecondValue;
+        }
+    }
+}
